db934df0ec71c5e0d2be6c259476eb0d7cc9165a
FreeInfoMarket <34045984+FreeInfoMarket@users.noreply.github.com>
2017-11-28 20:45:51 -0500

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,6 +1,6 @@
 $(function() {
 
-  $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+  $("#contactForm input,#contactForm textarea"){
     preventSubmit: true,
     submitError: function($form, event, errors) {
       // additional error messages or events
